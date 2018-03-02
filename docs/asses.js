c1172f2646de3895420c984895ece4928630d32c
Kujiy <kujiy@users.noreply.github.com>
2018-03-02 19:36:38 +0900

    TODO: make bookmarklet

---
+++ docs/asses.js
@@ -0,0 +1,56 @@
+$("select[id^=id_grade]").each(function(e) { $(this).val(10);});
+
+$("fieldset").each(function(e){ 
+    $this = $(this);
+    var textarea = $this.find("textarea");
+
+    $this.find("div[id^=id_dim_] p").each(function(e){ 
+        var id = $(this).attr('id');
+        
+        // console.log("textarea="+textarea);
+        var t = $(this).text();
+        var ptn1 = new RegExp(/^.*?Requirements /);
+        var ptn2 = new RegExp(/\s?(Did |Is |Are |Does )the /);
+        var s = t.replace(ptn1, "").replace(ptn2, "This ").replace("essay at least", "essay is at least");
+        var s = replace_past_tense(s, textarea);
+        // console.log(s);
+    });
+});
+
+function replace_past_tense(s, textarea) {
+    // "This student refer" の "refer" を取得
+    var verb = get_word(s,3);
+    verb = String(verb).replace(",", "");
+    // console.log("verb="+verb);
+    // referを 過去形 referred に変換
+    var past = get_past_tense(verb, s, textarea);
+}
+
+// get n-th word
+function get_word(s,n){
+    return s.split(/\s+/).slice(n-1,n);
+}
+
+function get_past_tense(verb, s, textarea) {
+    var url = "https://tensify.herokuapp.com/" + verb;
+
+    $.ajax({
+        async : false,
+        type: "GET",
+        url: url,
+        dataType: "text"
+    }).done(function(past) {
+        // console.log("res="+past);
+        update_textarea(s, verb, past, textarea);
+    });
+}
+
+function update_textarea(s, verb, past, textarea) {
+
+    // console.log("past="+past);
+
+    var ptn = new RegExp("(.*?)" + verb + "(.*)");
+    out = s.replace(ptn, "$1" + past + "$2").replace("?", ".");
+    console.log(out);
+    $(textarea).val(out);
+}
