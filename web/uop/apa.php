ad04bfde0254a1febab8e46f3eb8ddf1165e8c8c
Kujiy <kujiy@users.noreply.github.com>
2021-12-02 23:44:31 +0900

    analytics

--- web/uop/apa.php
+++ web/uop/apa.php
@@ -486,3 +486,12 @@ if (typeof(Storage) !== "undefined") {
 
 </script>
 
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-2TVVW83NFB"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-2TVVW83NFB');
+</script>
