394d551e125722f154ced10157d273e7bedae121
kuji <kujiy.fs@gmail.com>
2022-06-06 00:58:01 +0900

    short-answer-choose

--- docs/quiz2-choose.js
+++ docs/quiz2-choose.js
@@ -1,27 +1,56 @@
+function isGradedQuiz() {
+	let out = false;
+	 $("#page-navbar nav a").each((_i, o) => { 
+	 	if($(o).text().includes("Graded Quiz") || ($(o).text().includes("Final Exam") && !$(o).text().includes("Review Quiz"))) 
+	 		out = true;
+	 });
+	 return out;
+}
+var GRADED_QUIZ = isGradedQuiz();
+console.log("Graded quiz = " + GRADED_QUIZ);
+if(GRADED_QUIZ) {
+	console.log("GRADDDDDDDDDDDDDDDDDDDDDDDDDDDDDD");
+}
+
 function chooseCorrect(question, answer, ansObj) {
 	db.transaction(function(tx) {
-		tx.executeSql('select correct from quiz where question like ? and answer like ?', [question, answer],
+		let sql = "";
+		let arg = [];
+		if (answer) {
+			sql = 'select * from quiz where question like ? and answer like ?';
+			arg = [question, answer];
+		} else {
+			sql = 'select * from quiz where question like ? ';
+			arg = [question];
+		}
+		console.log(sql);
+		tx.executeSql(sql, arg,
 			(tx, results) => {
+				console.log("resul");
+				console.log(results);
 				if (results.rows.length === 0) {
 					/* check when no answer has been recorded */
 					tx.executeSql('select correct from quiz where question like ?', [question],
 						(tx, results) => {
 							if (results.rows.length === 0) {
 								console.log("--- because no-DB: " + answer);
-								$(ansObj).find("input:radio").prop("checked", true);
+								if (!GRADED_QUIZ) {
+									$(ansObj).find("input:radio").prop("checked", true);
+								}
 							}
-					})
+					});
 					return;
 				}
 				/* SUCCESS */
 				let hasChoosenCorrect = false;
 				for (i = 0; i < results.rows.length; i++) {
 					let item = results.rows.item(i);
-					console.log(question + " anwser=" + answer + " DB=" + item.correct);
+					console.log(item.question + " anwser=" + item.answer + " DB=" + item.correct);
 					if (item.correct === "true") {
-						console.log("--- because DB has True");
+						console.log("--- because DB has True. the correct answer from DB is " + item.answer);
 						hasChoosenCorrect = true;
 						$(ansObj).find("input:radio").prop("checked", true);
+						$(ansObj).val(item.answer); // shortanswer
 					} else if (item.correct === "false") {
 						/* skip */
 						$(ansObj).find("input:radio").attr("mymark", "incorrect");
@@ -70,9 +99,24 @@ function multichoice() {
 		const question = $(o).find(".qtext").text();
 
 		$(o).find(".answer>div").each((i, ansObj) => {
-			let hasChoosenCorrect = false;
 			const answer = $(ansObj).text();
 			chooseCorrect(question, answer, ansObj);
+		})
+	});
+	return res;
+}
+
+function shortanswer() {
+	$(".answernumber").remove();
+
+	let res = [];
+	$(".shortanswer .content").each((i, o) => {
+
+		const question = $(o).find(".qtext").text();
+		console.log(question);
+
+		$(o).find("input[type=text]").each((i, ansObj) => {
+			chooseCorrect(question, null, ansObj);
 		})
 	});
 	return res;
@@ -93,7 +137,7 @@ db.transaction(function(tx) {
 });
 
 
-res = [...truefalse(), ...multichoice()];
+res = [...truefalse(), ...multichoice(), ...shortanswer()];
 
 /* -----------------------------------*/
 	setTimeout(function() {
