58a269d951beaaf03a8e039b1f8fabb620420ce0
kujiy <kujiy.fs@gmail.com>
2023-02-10 22:18:28 +0900

    does not store and choose

--- docs/quiz2-choose.js
+++ docs/quiz2-choose.js
@@ -1,125 +1,127 @@
 function isGradedQuiz() {
-	let out = false;
-	 $("#page-navbar nav a").each((_i, o) => { 
-	 	if($(o).text().includes("Graded Quiz") || ($(o).text().includes("Final Exam") && !$(o).text().includes("Review Quiz"))) 
-	 		out = true;
-	 });
-	 return out;
+    let out = false;
+    $("#page-navbar nav a").each((_i, o) => {
+        if ($(o).text().includes("Graded Quiz") || ($(o).text().includes("Final Exam") && !$(o).text().includes("Review Quiz")))
+            out = true;
+    });
+    return out;
 }
 var GRADED_QUIZ = isGradedQuiz();
 console.log("Graded quiz = " + GRADED_QUIZ);
-if(GRADED_QUIZ) {
-	console.log("GRADDDDDDDDDDDDDDDDDDDDDDDDDDDDDD");
+if (GRADED_QUIZ) {
+    console.log("GRADDDDDDDDDDDDDDDDDDDDDDDDDDDDDD");
 }
 
 function chooseCorrect(question, answer, ansObj) {
-	db.transaction(function(tx) {
-		let sql = "";
-		let arg = [];
-		if (answer) {
-			sql = 'select * from quiz where question like ? and answer like ?';
-			arg = [question, answer];
-		} else {
-			sql = 'select * from quiz where question like ? ';
-			arg = [question];
-		}
-		console.log(sql);
-		tx.executeSql(sql, arg,
-			(tx, results) => {
-				console.log("resul");
-				console.log(results);
-				if (results.rows.length === 0) {
-					/* check when no answer has been recorded */
-					tx.executeSql('select correct from quiz where question like ?', [question],
-						(tx, results) => {
-							if (results.rows.length === 0) {
-								console.log("--- because no-DB: " + answer);
-								if (!GRADED_QUIZ) {
-									$(ansObj).find("input:radio").prop("checked", true);
-								}
-							}
-					});
-					return;
-				}
-				/* SUCCESS */
-				let hasChoosenCorrect = false;
-				for (i = 0; i < results.rows.length; i++) {
-					let item = results.rows.item(i);
-					console.log(item.question + " anwser=" + item.answer + " DB=" + item.correct);
-					if (item.correct === "true") {
-						console.log("--- because DB has True. the correct answer from DB is " + item.answer);
-						hasChoosenCorrect = true;
-						$(ansObj).find("input:radio").prop("checked", true);
-						$(ansObj).val(item.answer); // shortanswer
-					} else if (item.correct === "false") {
-						/* skip */
-						$(ansObj).find("input:radio").attr("mymark", "incorrect");
-					}
-				}
-				if (!hasChoosenCorrect) {
-					const probability = $(ansObj).find("input:radio").attr("mymark") !== "incorrect";
-					if (probability) {
-						console.log("--- because probability: " + answer);
-						$(ansObj).find("input:radio").prop("checked", true);
-					}
-				}
+    db.transaction(function(tx) {
+        let sql = "";
+        let arg = [];
+        if (answer) {
+            sql = 'select * from quiz where question like ? and answer like ?';
+            arg = [question, answer];
+        } else {
+            sql = 'select * from quiz where question like ? ';
+            arg = [question];
+        }
+        console.log(sql, arg);
+        tx.executeSql(sql, arg,
+            (tx, results) => {
+                console.log("result:");
+                console.log(results);
+                console.log("--- results.rows.length: " + results.rows.length);
 
-			},
-			function(err) {
-				/* ERROR */
-				console.log(err);
-				return false;
-			})
-	})
+                if (results.rows.length === 0) {
+                	console.log("--- results.rows.length : got in");
+                    /* check when no answer has been recorded */
+                    tx.executeSql('select correct from quiz where question like ?', [question],
+                        (tx, results) => {
+                		console.log("--- results.rows.length - inside: " + results.rows.length);
+                            if (results.rows.length === 0) {
+                                console.log("--- because no-DB: " + answer);
+                                if (!GRADED_QUIZ) {
+                                	console.log("checked: " + answer);
+                                    $(ansObj).find("input:radio").prop("checked", true);
+                                }
+                            }
+                        });
+                }
+                /* SUCCESS */
+                let hasChoosenCorrect = false;
+                for (i = 0; i < results.rows.length; i++) {
+                    let item = results.rows.item(i);
+                    console.log(item.question + " anwser=" + item.answer + " DB=" + item.correct);
+                    if (item.correct === "true") {
+                        console.log("--- because DB has True. the correct answer from DB is " + item.answer);
+                        hasChoosenCorrect = true;
+                        $(ansObj).find("input:radio").prop("checked", true);
+                        $(ansObj).val(item.answer); // shortanswer
+                    } else if (item.correct === "false") {
+                        /* skip */
+                        $(ansObj).find("input:radio").attr("mymark", "incorrect");
+                    }
+                }
+                if (!hasChoosenCorrect) {
+                	console.log($(ansObj).find("input:radio").attr("mymark"))
+                    const probability = $(ansObj).find("input:radio").attr("mymark") !== "incorrect";
+                    if (probability) {
+                        console.log("--- because probability: " + answer);
+                        $(ansObj).find("input:radio").prop("checked", true);
+                    } else {
+                    	console.log("--- no probablitiy")
+                    }
+                }
+
+            },
+            function(err) {
+                /* ERROR */
+                console.log(err);
+                return false;
+            })
+    })
 }
 
 
-function truefalse() {
-	let res = [];
-	$(".truefalse .content").each((i, o) => {
+async function truefalse() {
+    $(".truefalse .content").each((i, o) => {
 
-		const question = $(o).find(".qtext").text();
-		console.log(question);
-		$(o).find(".answer>div").each((i, ansObj) => {
-			const answer = $(ansObj).find("label").text();
-			console.log(answer);
-			chooseCorrect(question, answer, ansObj);
-		})
-	});
-	return res;
+        const question = $(o).find(".qtext").text();
+        console.log(question);
+        $(o).find(".answer>div").each((i, ansObj) => {
+            const answer = $(ansObj).find("label").text();
+            console.log(answer);
+            chooseCorrect(question, answer, ansObj);
+        })
+    });
 }
 
 
-function multichoice() {
-	$(".answernumber").remove();
+async function multichoice() {
+    $(".answernumber").remove();
 
-	let res = [];
-	$(".multichoice .content").each((i, o) => {
+    $(".multichoice .content").each((i, o) => {
 
-		const question = $(o).find(".qtext").text();
+        const question = $(o).find(".qtext").text();
 
-		$(o).find(".answer>div").each((i, ansObj) => {
-			const answer = $(ansObj).text();
-			chooseCorrect(question, answer, ansObj);
-		})
-	});
-	return res;
+        $(o).find(".answer>div").each((i, ansObj) => {
+            const answer = $(ansObj).text();
+            console.log(answer);
+            chooseCorrect(question, answer, ansObj);
+        })
+    });
 }
 
-function shortanswer(answerType) {
-	$(".answernumber").remove();
+async function shortanswer(answerType) {
+    $(".answernumber").remove();
 
-	let res = [];
-	$(answerType + " .content").each((i, o) => {
+    $(answerType + " .content").each((i, o) => {
 
-		const question = $(o).find(".qtext").text();
-		console.log(question);
+        const question = $(o).find(".qtext").text();
+        console.log(question);
 
-		$(o).find("input[type=text]").each((i, ansObj) => {
-			chooseCorrect(question, null, ansObj);
-		})
-	});
-	return res;
+        $(o).find("input[type=text]").each((i, ansObj) => {
+            chooseCorrect(question, null, ansObj);
+        })
+    });
 }
 
 
@@ -133,18 +135,21 @@ var db = openDatabase(name, version, description, size);
 
 /* create table */
 db.transaction(function(tx) {
-	tx.executeSql("  create table if not exists quiz (     id integer primary key autoincrement,     question varchar,     answer varchar,     correct bool   ) ")
+    tx.executeSql("  create table if not exists quiz (     id integer primary key autoincrement,     question varchar,     answer varchar,     correct bool   ) ")
 });
 
 
-res = [...truefalse(), ...multichoice(), ...shortanswer(".shortanswer"), ...shortanswer(".numerical")];
+res = Promise.all([truefalse(), multichoice(), shortanswer(".shortanswer"), shortanswer(".numerical")]).then(() => {
 
-
-			$("html, body").animate({
-			scrollTop: $(document).height()
-			}, "slow");
-			
-/* -----------------------------------*/
+	/* -----------------------------------*/
 	setTimeout(function() {
-			$("form .submitbtns input[name=next]").click();
-	}, 4000);
+        $(document).scrollTop($(document).height());
+	    $("form .submitbtns input[name=next]").click();
+	}, 2000);
+
+
+
+
+})
+
+
--- docs/quiz2-store.js
+++ docs/quiz2-store.js
@@ -1,146 +1,138 @@
-
-
 /* Store results ----------------------------------------- */
-  /* store funcs ------------------------------------------- */
+/* store funcs ------------------------------------------- */
 function truefalse() {
-  let res = [];
-  $(".truefalse .content").each((i, o) => {
+    let res = [];
+    $(".truefalse .content").each((i, o) => {
 
-    const question =  $(o).find(".qtext").text();
-    console.log(question);
+        const question = $(o).find(".qtext").text();
+        console.log(question);
 
-    $(o).find(".answer>div").each((i, ansObj) => {
-         const answer = $(ansObj).find("label").text();
-         let correct = $(ansObj).find("i").attr("title");
-         if (correct  === "Correct") {
-         	correct = true;
-         } else if (correct === "Incorrect") {
-         	correct = false;
-         } else {
-           return
-         }
-         res.push({
-	       question: question,
-           answer: answer,
-           correct: correct
-         })
+        $(o).find(".answer>div").each((i, ansObj) => {
+            const answer = $(ansObj).find("label").text();
+            console.log("answer=" + answer);
+            let correct = $(ansObj).find("i").attr("title");
+            console.log("correct=" + correct);
+
+            if (correct === "Correct") {
+                correct = true;
+            } else if (correct === "Incorrect") {
+                correct = false;
+            } else {
+                return
+            }
+            res.push({
+                question: question,
+                answer: answer,
+                correct: correct
+            })
+        });
     });
-    
-    let correct = "";
-    res.forEach((o) => {
-        if (o.correct !== null) correct = o.correct
-    });
-    res.forEach((o) => {
-        if (o.correct === null) o.correct = !correct
-    });
-    res = res.filter(o => o.correct);    
-  });
-  return res;
+    return res;
 }
 
 
 function multichoice() {
-	$( ".answernumber" ).remove();
+    $(".answernumber").remove();
 
-  let res = [];
-  $(".multichoice .content").each((i, o) => {
+    let res = [];
+    $(".multichoice .content").each((i, o) => {
 
-    const question =  $(o).find(".qtext").text();
-    console.log(question);
+        const question = $(o).find(".qtext").text();
+        console.log(question);
 
-    $(o).find(".answer>div").each((i, ansObj) => {
-         const answer = $(ansObj).text();
-         let correct = $(ansObj).find("i").attr("title");
-         if (correct  === "Correct") {
-         	correct = true;
-         } else if (correct === "Incorrect") {
-         	correct = false;
-         } else {
-           return
-         }
-         res.push({
-	       question: question,
-           answer: answer,
-           correct: correct
-         })
-    })
-    
-  });
-  return res;
+        $(o).find(".answer>div").each((i, ansObj) => {
+            const answer = $(ansObj).text();
+            let correct = $(ansObj).find("i").attr("title");
+            if (correct === "Correct") {
+                correct = true;
+            } else if (correct === "Incorrect") {
+                correct = false;
+            } else {
+                return
+            }
+            res.push({
+                question: question,
+                answer: answer,
+                correct: correct
+            })
+        })
+
+    });
+    return res;
 }
 
 
 
 function shortanswer(answerType) {
-  $( ".answernumber" ).remove();
+    $(".answernumber").remove();
 
-  let res = [];
-  $(answerType + " .content").each((i, o) => {
+    let res = [];
+    $(answerType + " .content").each((i, o) => {
 
-    const question =  $(o).find(".qtext").text();
-    console.log(question);
+        const question = $(o).find(".qtext").text();
+        console.log(question);
 
-    $(o).find("input[type=text]").each((i, ansObj) => {
-         const answer = $(ansObj).attr("value");
-         console.log(answer);
+        $(o).find("input[type=text]").each((i, ansObj) => {
+            const answer = $(ansObj).attr("value");
+            console.log(answer);
 
-         let correct = $(o).find("i").attr("title");
-         console.log(correct);
-         if (correct  === "Correct") {
-         	correct = true;
-         } else if (correct === "Incorrect") {
-         	correct = false;
-         } else {
-           return
-         }
-         console.log(correct);
-         res.push({
-	       question: question,
-           answer: answer,
-           correct: correct
-         })
-    })
-    
-  });
-  return res;
+            let correct = $(o).find("i").attr("title");
+            console.log(correct);
+            if (correct === "Correct") {
+                correct = true;
+            } else if (correct === "Incorrect") {
+                correct = false;
+            } else {
+                return
+            }
+            console.log(correct);
+            res.push({
+                question: question,
+                answer: answer,
+                correct: correct
+            })
+        })
+
+    });
+    return res;
 }
 
 
 /* 未完成。新しいDB tableが必要 */
 function multianswer() {
-  $( ".answernumber" ).remove();
+    $(".answernumber").remove();
 
-  let res = [];
-  $(".multianswer .content").each((i, o) => {
+    let res = [];
+    $(".multianswer .content").each((i, o) => {
 
-    const question =  $(o).find("p").text();
-    console.log(question);
-    let answers = [];
-    let corrects = [];
-    $(o).find(".subquestion input").each((i, ansObj) => {
-         const answer = $(ansObj).val();
-         console.log(answer)
-         correct = $(ansObj).find("i").attr("title");
-         if (correct  === "Correct") {
-           corrects.push(true);
-           answers.push(answer)
-         } else if (correct === "Incorrect") {
-           corrects.push(false);
-           answers.push('')
-         } else {
-           corrects.push(null);
-           answers.push('')
-         }
-         
-    })
-    res.push({
-      question: question,
-        answer: answers.join(","),
-        correct: corrects.every(boolean => boolean === true)
-    })
-    
-  });
-  return res;
+        const question = $(o).find("p").text();
+        console.log(question);
+        let answers = [];
+        let corrects = [];
+        $(o).find(".subquestion input").each((i, ansObj) => {
+            const answer = $(ansObj).val();
+            console.log(answer)
+            correct = $(ansObj).find("i").attr("title");
+            if (correct === "Correct") {
+                corrects.push(true);
+                answers.push(answer)
+            } else if (correct === "Incorrect") {
+                corrects.push(false);
+                answers.push('')
+            } else {
+                corrects.push(null);
+                answers.push('')
+            }
+
+        })
+        res.push({
+            question: question,
+            answer: answers.join(","),
+            correct: corrects.every(boolean => boolean === true)
+        })
+
+    });
+    return res;
 }
 
 
@@ -152,22 +144,22 @@ var description = 'Web SQL Database';
 var size = 2 * 1024 * 1024;
 var db = openDatabase(name, version, description, size);
 
-  /* create table */
-  db.transaction(function(tx){
+/* create table */
+db.transaction(function(tx) {
     tx.executeSql("  create table if not exists quiz (     id integer primary key autoincrement,     question varchar,     answer varchar,     correct bool   ) ")
-  });
+});
 
 
 res = [...truefalse(), ...multichoice(), ...shortanswer(".shortanswer"), ...shortanswer(".numerical"), ...multianswer()];
 
 function callback() {
-	console.log("inserted.");
+    console.log("inserted.");
 }
 res.forEach(o => {
-	console.log('insert into quiz (question, answer, correct) values (?, ?, ?)', [o.question, o.answer, o.correct]);
-	db.transaction(function (tx) {
-	  tx.executeSql('insert into quiz (question, answer, correct) values (?, ?, ?)', [o.question, o.answer, o.correct], callback)
-	})
+    console.log('insert into quiz (question, answer, correct) values (?, ?, ?)', [o.question, o.answer, o.correct]);
+    db.transaction(function(tx) {
+        tx.executeSql('insert into quiz (question, answer, correct) values (?, ?, ?)', [o.question, o.answer, o.correct], callback)
+    })
 });
 
 
@@ -177,4 +169,3 @@ res.forEach(o => {
 const exp = /http.*cmid=(\d+)/ig;
 const url = document.URL.replace(exp, "https://my.uopeople.edu/mod/quiz/view.php?id=$1");
 window.open(url);
-
