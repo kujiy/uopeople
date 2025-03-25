8ded455f4a5121b2c6702d69d26b548f70a63adf
Kujiy <kujiy@users.noreply.github.com>
2025-03-25 21:42:45 +0900

    Create assessment.js

---
+++ docs/assessment.js
@@ -0,0 +1,1 @@
+javascript:(function() {var num = prompt('select menu option value 10 to 0 or yes?%27, %2710%27);  document.querySelectorAll('select.custom-select').forEach((select) => select.value = num)})();
